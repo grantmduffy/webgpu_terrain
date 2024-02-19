d48da7a90cfb7a1d2b4fb658e766b055ad9ac6f1
grant <grantmduffy@gmail.com>
2024-02-18 19:01:01 -0800

    rescale z

--- multi_fbo_test.js
+++ multi_fbo_test.js
@@ -322,6 +322,8 @@ precision highp sampler2D;
 uniform mat4 M_camera;
 uniform sampler2D other_t;
 
+#define z_scale 0.1
+
 in vec2 vert_pos;
 out vec3 xyz;
 out vec2 xy;
@@ -329,7 +331,7 @@ out vec2 xy;
 void main(){
     xy = vert_pos;
     vec4 other = texture(other_t, xy);
-    float elevation = other.z;
+    float elevation = other.z * z_scale;
     xyz = vec3(vert_pos, elevation);
     gl_Position = M_camera * vec4(xyz, 1.);
 }
